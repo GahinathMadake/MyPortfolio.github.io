e64bc5445a3205e919b54e816b5ee6cbcbe9909f
gahinathmadake <gahinathmadake@gmail.com>
2023-10-17 22:20:36 +0530

    Change

--- index.js
+++ index.js
@@ -9,28 +9,47 @@ const active = document.querySelector(".header")
 
 
 
-const text = document.querySelector(".second_text");
-const textLoad = () => {             
-    setTimeout(() => {                 
-        text.textContent = "the lover of Coding";             
-    }, 0);     
 
-    setTimeout(() => {                 
-        text.textContent = "Web Developer";
-    }, 6000);                               // 1s = 1000 millisecond             
-    
-    setTimeout(() => {                 
-        text.textContent = "Passionate Programmer";             
-    }, 12000);
-    
-    setTimeout(() => {                 
-        text.textContent = "Tech Enthusiast";             
-    }, 18000);
-}                  
-    
-    textLoad();         
-    setInterval(textLoad,24000);           // define the next iterate of the loop interval
-
-
-
+ const TypingText = document.querySelector(".typed-text");
+ const AutoTyping = document.querySelector(".TypeCursor");
+  
+ const textArray = ["the lover of Coding", "Web Developer", "Passionate Programmer",
+                    "Tech Enthusiast"];
+ const typingDelay = 100;
+ const erasingDelay = 100;
+ const newTextDelay = 2000; 
+ let textArrayIndex = 0;
+ let charIndex = 0;
+  
+ function type() {
+   if (charIndex < textArray[textArrayIndex].length) {
+     if(!AutoTyping.classList.contains("typing")) AutoTyping.classList.add("typing");
+     TypingText.textContent += textArray[textArrayIndex].charAt(charIndex);
+     charIndex++;
+     setTimeout(type, typingDelay);
+   } 
+   else {
+     AutoTyping.classList.remove("typing");
+       setTimeout(erase, newTextDelay);
+   }
+ }
+  
+ function erase() {
+     if (charIndex > 0) {
+     if(!AutoTyping.classList.contains("typing")) AutoTyping.classList.add("typing");
+     TypingText.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+     charIndex--;
+     setTimeout(erase, erasingDelay);
+   } 
+   else {
+     AutoTyping.classList.remove("typing");
+     textArrayIndex++;
+     if(textArrayIndex>=textArray.length) textArrayIndex=0;
+     setTimeout(type, typingDelay + 1100);
+   }
+ }
+  
+ document.addEventListener("DOMContentLoaded", function() { 
+   if(textArray.length) setTimeout(type, newTextDelay + 250);
+ });
 
